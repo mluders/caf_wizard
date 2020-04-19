5fc02bee87a2a5ffff2cab4226a5368b5f6a8422
Miles Luders <mpluders@gmail.com>
2020-04-18 20:19:19 -0800

    Create basic front page

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,5 @@
 class MenusController < ApplicationController
   def index
-    @menus = Menu::CAFE_UIDS.map do |cafe_name, cafe_uid|
-      [cafe_name, Menu.order(:created_at).where(cafe_uid: cafe_uid).last]
-    end.to_h
+    @menus = Menu.most_recent
   end
 end
